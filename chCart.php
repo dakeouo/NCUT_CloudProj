2b3de9197c68a2d4f3f568009c76a741076cdc7a
Louis Hong <dake852@gmail.com>
2019-05-15 11:52:16 +0800

    update chCart.php

--- chCart.php
+++ chCart.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+include_once "model/dataset.php";
 include_once "model/cartModel.php";
+$dataset = new Dataset();
 $cartModel = new CartModel();
+
 $meth = $_GET['method'];
 $pid = $_GET['pid'];
 $token = $_SESSION['cart_token'];
